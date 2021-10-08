bcafcd3b7dbc78f956785f1fc9f99165d98a8ad9
Oqex <75790154+Oqex@users.noreply.github.com>
2021-10-08 14:18:30 +0300

    numerolista

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ functin arvoNumero(alaraja, ylaraja) {
     return Math.floor(ylaraja * Math.random()) + alaraja
 }
 
-let NUMEROLISTA = [
-    arvoNumero(0,5),
-    arvoNumero(10,15),
-    arvoNumero(100,500),
-    arvoNumero(10,20),
-    arvoNumero(0,5),
-]
 
 app.get('/', function (req, res) {
-    res.json(NUMEROLISTA)
+    let numerolista = [
+         arvoNumero(0,5),
+         arvoNumero(10,15),
+         arvoNumero(100,500),
+         arvoNumero(10,20),
+         arvoNumero(0,5),
+    ]
+
+    res.json(numerolista)
 })
  
  app.listen(PORT)
