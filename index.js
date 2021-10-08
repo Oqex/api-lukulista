d4ad9305b30519fe16706002fbf82ab5c8332f0e
Oqex <75790154+Oqex@users.noreply.github.com>
2021-10-08 14:22:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-functin arvoNumero(alaraja, ylaraja) {
+function arvoNumero(alaraja, ylaraja) {
     return Math.floor(ylaraja * Math.random()) + alaraja
 }
 
