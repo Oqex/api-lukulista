efd3e870107242f7a403b5b60d0c8d134fef3957
Oqex <75790154+Oqex@users.noreply.github.com>
2021-10-08 14:14:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+functin arvoNumero(alaraja, ylaraja) {
+    return Math.floor(ylaraja * Math.random()) + alaraja
+}
+
+let NUMEROLISTA = [
+    arvoNumero(0,5),
+    arvoNumero(10,15),
+    arvoNumero(100,500),
+    arvoNumero(10,20),
+    arvoNumero(0,5),
+]
+
 app.get('/', function (req, res) {
     res.json(['1 5 8 2 5'])
 })
