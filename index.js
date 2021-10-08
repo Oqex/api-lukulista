2a5c2bfb8bb533610476fcea92d25b23bae44c1f
Oqex <75790154+Oqex@users.noreply.github.com>
2021-10-08 14:21:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ functin arvoNumero(alaraja, ylaraja) {
 }
 
 
+
 app.get('/', function (req, res) {
     let numerolista = [
-         arvoNumero(0,5),
-         arvoNumero(10,15),
-         arvoNumero(100,500),
-         arvoNumero(10,20),
-         arvoNumero(0,5),
+        arvoNumero(0,5),
+        arvoNumero(10,15),
+        arvoNumero(100,500),
+        arvoNumero(10,20),
+        arvoNumero(0,5),
     ]
-
     res.json(numerolista)
 })
  
